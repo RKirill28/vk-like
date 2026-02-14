b1e9a324ec815f31c7bd29276f800fa95887e810
RKirill28 <pavlov26.2008@gmail.com>
2026-02-15 04:14:14 +0700

    changed vk api class. now vk api is fetch function for make request and get parsed result(pydantic model)

--- vk_api.py
+++ vk_api.py
@@ -26,28 +26,8 @@ class VkApiError(Exception):
 class VkApi:
     based_url = "https://api.vk.com/method"
 
-    _post_methods = [
-        "likes.add",
-        "captchaNotRobot.endSession",
-        "captchaNotRobot.check",
-        "captchaNotRobot.componentDone",
-        "captchaNotRobot.settings",
-    ]
-    _get_methods = ["wall.get"]
-
-    def __init__(self, request_session: requests.Session, auth_session: AuthData):
+    def __init__(self, request_session: requests.Session):
         self._session = request_session
-        self._data = {}
-        self._default_data = {"access_token": auth_session.access_token}
-
-        self._captcha_params = {"v": "5.131"}
-        self._liker_params = {"v": "5.269", "client_id": auth_session.client_id}
-        self._access_token = auth_session.access_token
-        self._expires = auth_session.expires
-        self._cookies = auth_session.cookies
-
-    def _handle_error(self, error: ErrorModel) -> None:
-        pass
 
     def fetch_and_get_result(
         self, result_model: Type[T], method: str, params: dict, data: dict | None = None
@@ -55,12 +35,14 @@ class VkApi:
         res = self._session.post(
             self.based_url + "/" + method, params=params, data=data
         )
-        return self.get_res(res, result_model)
+        return self._parse_response(res, result_model)
 
-    def get_res(self, res: requests.Response, model: Type[T]) -> T | ErrorModel:
+    def _parse_response(
+        self, response: requests.Response, model: Type[T]
+    ) -> T | ErrorModel:
         try:
-            assert res.status_code == 200
-            res_json = res.json()
+            assert response.status_code == 200
+            res_json = response.json()
             if res_json.get("error") is not None:
                 return ErrorModel.model_validate(res_json["error"])
 
@@ -68,100 +50,15 @@ class VkApi:
                 return model.model_validate(res_json["response"])
             else:
                 return model.model_validate(res_json)
-        except json.JSONDecodeError:
-            raise VkApiError(f"Could not get JSON from response: {res.text}")
+        except json.JSONDecodeError as e:
+            raise VkApiError(
+                f"Could not get JSON from response: {response.text}\nError: {str(e)}"
+            )
         except pydantic.ValidationError as e:
             raise VkApiError(
-                f"Could not create ErrorModel\nResponse: {pprint(res.text)}\nValidateError: {pprint(e)}"
+                f"Could not create ErrorModel\nResponse: {response.text}\nValidateError: {str(e)}"
             )
         except Exception as e:
             raise VkApiError(
-                f"Could not get response, status_code: {res.status_code}\nerror: {e}"
+                f"Could not get response, status_code: {response.status_code}\nError: {e}"
             )
-
-    def get_wall(self, owner_id: int, start_from: int):
-        res = self._session.get(
-            self.based_url + "/wall.get",
-            params=self._liker_params,
-            data={
-                "extended": "1",
-                "filters": "post",
-                "filter": "owner",
-                "domain": "-" + str(owner_id),
-                "start_from": str(start_from),
-                "count": "100",
-                **self._default_data,
-            },
-        )
-        try:
-            assert res.status_code == 200
-            res_json = res.json()
-            if res_json.get("error") is not None:
-                return ErrorModel(**res_json)
-
-            return PostsResponseModel(**res_json)
-        except json.JSONDecodeError:
-            raise VkApiError(f"Could not get JSON from response: {res.text}")
-        except pydantic.ValidationError as e:
-            raise VkApiError(
-                f"Could not create ErrorModel\nResponse: {pprint(res.text)}\nValidateError: {pprint(e)}"
-            )
-        except Exception as e:
-            raise VkApiError(
-                f"Could not get response, status_code: {res.status_code}\nerror: {e}"
-            )
-
-    def add_like(self, post_model: PostModel) -> ErrorModel | None:
-        res = self._session.post(
-            self.based_url + "/likes.add",
-            params=self._liker_params,
-            data={
-                **self._default_data,
-                **post_model.model_dump(),
-                "ref": "group",
-                "reaction_id": 0,
-            },
-        )
-        try:
-            assert res.status_code == 200
-            res_json = res.json()
-            if res_json.get("error") is not None:
-                return ErrorModel(**res_json)
-        except json.JSONDecodeError:
-            raise VkApiError(f"Could not get JSON from response: {res.text}")
-        except pydantic.ValidationError as e:
-            raise VkApiError(
-                f"Could not create ErrorModel\nResponse: {pprint(res.text)}\nValidateError: {pprint(e)}"
-            )
-        except Exception as e:
-            raise VkApiError(
-                f"Could not get response, status_code: {res.status_code}\nerror: {e}"
-            )
-
-    def _fetch(
-        self, method: str, data: dict, params: dict, except_response_model: T | None
-    ) -> T | dict:
-        if method in self._post_methods:
-            res = self._session.post(
-                self.based_url + "/" + method, params=params, data=data
-            )
-        elif method in self._get_methods:
-            res = self._session.get(self.based_url + "/" + method, params=params)
-        else:
-            raise VkApiError("Could not find method:", method)
-
-        try:
-            res_json: dict = res.json()
-            if except_response_model is not None:
-                return Type[except_response_model](**res_json).model_dump()
-            return res_json
-        except TypeError:
-            return ErrorResponseModel(**res_json).model_dump()
-        except pydantic.ValidationError as e:
-            raise VkApiError("Could not get data from JSON, because:\n", e)
-        except Exception as e:
-            raise VkApiError("Unknown error, I could not get JSON from response:\n", e)
-
-
-class PostsApi(VkApi[PostsResponseModel]):
-    pass
